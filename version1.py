e5f93f2687282933fa3ac73ebac8443348ab7e9d
Sulav Shrestha <stha.sulabh@gmail.com>
2024-02-05 11:00:02 -0500

    Update multiplication function for new version

--- version1.py
+++ version1.py
@@ -1,11 +1,18 @@
 import numpy as np
 
+VERSION = 1.2
+
 A = [[6, 7],
       [8, 9]]
-
       
 B = [[1, 3],
       [5, 7]]
 
-def math_multiply(A, B):
-    return (np.matmul(A,B))
+if VERSION == 1.1:
+    def product(A, B):
+        return (np.matmul(A,B))
+elif VERSION == 1.2:
+    def product(A, B):
+        return (np.dot(A,B))
+else:
+    print(f"Please specify version")
